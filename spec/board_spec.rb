b9c993bdd1dcb848120cb535832902b418653201
Tanner Dale <tanner@tannerdale.com>
2021-07-14 17:19:10 -0500

    Add validation helper methods tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -60,4 +60,53 @@ RSpec.describe Board do
       expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be(true)
     end
   end
+
+  context 'helper methods' do
+    board = Board.new
+    
+    it 'can format coordinates' do
+      expect(board.make_coords(["A1", "B1"])).to eq([["A", "1"], ["B", "1"]])
+    end
+
+    context 'validation steps' do
+      valid_coords_1 = board.make_coords(["A1", "A2", "A3"])
+      valid_coords_2 = board.make_coords(["A1", "B1", "C1"])
+
+      invalid_coords_1 = board.make_coords(["A1", "A2", "A4"])
+      invalid_coords_2 = board.make_coords(["A1", "B1", "D1"])
+      invalid_coords_3 = board.make_coords(["A1", "B2", "A3"])
+      invalid_coords_4 = board.make_coords(["A1", "B2", "C1"])
+
+
+      it 'can check same row' do
+        expect(board.same_row?(valid_coords_1, valid_coords_1.first)).to be(true)
+        expect(board.same_row?(invalid_coords_3, invalid_coords_3.first)).to be(false)
+      end
+
+      it 'can check same column' do
+        expect(board.same_column?(valid_coords_2, valid_coords_2.first)).to be(true)
+        expect(board.same_column?(invalid_coords_4, invalid_coords_4.first)).to be(false)
+      end
+
+      it 'can check no row gaps' do
+        expect(board.no_row_gaps?(valid_coords_1)).to be(true)
+        expect(board.no_row_gaps?(invalid_coords_1)).to be(false)
+      end
+
+      it 'can check no column gaps' do
+        expect(board.no_column_gaps?(valid_coords_2)).to be(true)
+        expect(board.no_column_gaps?(invalid_coords_2)).to be(false)
+      end
+
+      it 'can check for no gaps anywhere' do
+        expect(board.consecutive?(valid_coords_2)).to be(true)
+        expect(board.consecutive?(invalid_coords_2)).to be(false)
+      end
+
+      it 'can verify its in a line' do
+        expect(board.in_a_line?(valid_coords_1, valid_coords_1.first)).to be(true)
+        expect(board.in_a_line?(invalid_coords_3, invalid_coords_3.first)).to be(false)
+      end
+    end
+  end
 end
