063389ef744d087644450e17e0a3c4b910924a2d
Ida Olson <idaolson@gmail.com>
2021-07-16 13:48:59 -0600

    Move placement validator to module

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,9 @@
+require "./lib/placement_validator"
+
 class Board
+  include PlacementValidator
   attr_reader :cells
+
   def initialize
     @cells = make_cells
   end
@@ -25,45 +29,13 @@ class Board
 
     return false if not_empty?(coords)
 
-    in_a_line?(coords, coords.first) && consecutive?(coords)
+    PlacementValidator.valid?(coords)
   end
 
   def not_empty?(coords)
     coords.any? do |coord|
       @cells[coord].ship
     end
-  end
-
-  def in_a_line?(coords, first_coord)
-    same_row?(coords, first_coord[0]) || same_column?(coords, first_coord[1])
-  end
-
-  def same_row?(coords, letter)
-    coords.all? do |coord|
-      coord.start_with?(letter)
-    end
-  end
-
-  def same_column?(coords, num)
-    coords.all? do |coord|
-      coord.end_with?(num)
-    end
-  end
-
-  def consecutive?(coords)
-    no_row_gaps?(coords) || no_column_gaps?(coords)
-  end
-
-  def no_column_gaps?(coords)
-    columns = coords.map { |coord| coord[0] }
-
-    columns == (columns.first..columns.last).to_a
-  end
-
-  def no_row_gaps?(coords)
-    rows = coords.map { |coord| coord[1] }
-
-    rows == (rows.first..rows.last).to_a
   end
 
   def place(ship, coords)
