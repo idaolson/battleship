5ff57076dd93ba23de265bdc2179ad93e0490c07
Ida Olson <idaolson@gmail.com>
2021-07-18 14:42:45 -0600

    Move turn processing to module

--- lib/game.rb
+++ lib/game.rb
@@ -6,11 +6,13 @@ require './lib/random_ship_placer'
 require './lib/player_ship_placement'
 require './lib/ship_generator'
 require './lib/shot_processor'
+require './lib/game_processor'
 
 class Game
   include RandomShipPlacer
   include PlayerShipPlacement
   include ShipGenerator
+  include GameProcessor
   include GameBoardsMaker
   include ShotProcessor
 
@@ -28,72 +30,26 @@ class Game
   end
 
   def run_game
-    make_boards
+    assign_boards
     assign_ships
     place_ships(:computer)
     player_ship_placement
-
-    loop do
-      process_turns
-      if winner
-        puts winner
-        break
-      end
-    end
-
+    game_loop
     start_game
   end
 
-  def winner
-    return "You won!" if player_win?
-    return "I won!" if computer_win?
+  def assign_boards
+    data = make_boards
+    @dimensions = data[:dimensions]
+    @player_board = data[:player_board]
+    @computer_board = data[:computer_board]
+    @available_shots = data[:available_shots]
   end
 
-  def player_win?
-    @computer_ships.all? { |ship| ship.sunk? }
-  end
-
-  def computer_win?
-    @player_ships.all? { |ship| ship.sunk? }
-  end
-
-  def process_turns
-    puts display_boards
-    player_result = process_player_shot
-    computer_result = process_computer_shot
-    puts display_results(player_result, computer_result)
-    sunken_ship
-  end
-
-  def display_results(player_result, computer_result)
-    [
-      "Your shot on #{player_result.first} was a #{player_result.last}.",
-      "My shot on #{computer_result.first} was a #{computer_result.last}."
-    ].join("\n")
-  end
-
-  def sunken_ship
-    if computer_ship_sunk?
-      puts "You sunk one of my ships!"
-    end
-
-    if player_ship_sunk?
-      puts "I sunk one of your ships!"
-    end
-  end
-
-  def player_ship_sunk?
-    if @player_ships.any? { |ship| ship.sunk? }
-      @player_ships.delete_if { |ship| ship.sunk? }
-      return true
-    end
-  end
-
-  def computer_ship_sunk?
-    if @computer_ships.any? { |ship| ship.sunk? }
-      @computer_ships.delete_if { |ship| ship.sunk? }
-      return true
-    end
+  def assign_ships
+    ships = make_ships
+    @player_ships = ships
+    @computer_ships = ships
   end
 
   def display_boards
