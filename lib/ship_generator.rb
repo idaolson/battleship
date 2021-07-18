c1b6943a6253aa453399162169e4073913e39ecf
Ida Olson <idaolson@gmail.com>
2021-07-18 14:48:48 -0600

    Remove instance variable creation, rename

--- lib/ship_generator.rb
+++ lib/ship_generator.rb
@@ -1,15 +1,14 @@
 module ShipGenerator
   extend self
 
-  def assign_ships
+  def make_ships
     if custom_ships?
       ships = make_custom_ships(@dimensions.max)
     else
       ships = [Ship.new("cruiser", 3), Ship.new("submarine", 2)]
     end
 
-    @player_ships = ships
-    @computer_ships = ships
+    ships
   end
 
   def custom_ships?
@@ -30,18 +29,20 @@ module ShipGenerator
 
   def make_custom_ships(max_dimension)
     ships_available = max_dimension / 2
+    puts "You have up to #{ships_available} ships available to make."
 
     ships = []
     while ships_available > 0
-      ships << create_ship(ships_available, max_dimension)
+      ships << create_ship(max_dimension)
       ships_available -= 1
-      break if no_more_ships?
+      break if no_more_ships?(ships_available)
     end
 
     ships
   end
 
-  def no_more_ships?
+  def no_more_ships?(ships_available)
+    puts "You have up to #{ships_available} ships available to make."
     puts "Make another ship? (y/n)"
     print "> "
 
@@ -56,32 +57,35 @@ module ShipGenerator
      response == "n"
   end
 
-  def create_ship(ships_available, max_dimension)
-    puts "You have up to #{ships_available} ships available to make."
+  def create_ship(max_dimension)
+    Ship.new(get_name, get_length(max_dimension))
+  end
+
+  def get_name
     puts "Enter ship name:"
     print "> "
 
     name = gets.chomp
-
     while name == ""
       "Enter ship name:"
       print "> "
-
       name = gets.chomp
     end
 
+    name
+  end
+
+  def get_length(max_dimension)
     puts "Enter ship length (between 2 and #{max_dimension}):"
     print "> "
 
     length = gets.chomp.to_i
-
     while !(2..max_dimension).include?(length) do
       puts "Invalid input. Enter ship length (between 2 and #{max_dimension}):"
       print "> "
-
       length = gets.chomp.to_i
     end
 
-    Ship.new(name, length)
+    length
   end
 end
