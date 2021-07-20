c52fa98a6dd5f3f55ea93e6aeefbfb0157c11164
Tanner Dale <tanner@tannerdale.com>
2021-07-20 13:39:57 -0500

    Change validation method to use ordinal values

--- lib/placement_validator.rb
+++ lib/placement_validator.rb
@@ -2,39 +2,12 @@ module PlacementValidator
   extend self
 
   def valid?(coords)
-    in_a_line?(coords, coords.first) && consecutive?(coords)
-  end
+    coords = coords.map { |coord| coord.bytes }
+    coords[..-2].each_with_index.all? do |coord, i|
+      next_coord = coords[i + 1]
 
-  def in_a_line?(coords, first_coord)
-    number = first_coord.scan(/\d+/).first
-    same_row?(coords, first_coord[0]) || same_column?(coords, number)
-  end
+      result = [next_coord.first - coord.first, next_coord.last - coord.last]
 
-  def same_row?(coords, letter)
-    coords.all? do |coord|
-      coord.start_with?(letter)
+      result == [1, 0] || result == [0, 1]
     end
   end
-
-  def same_column?(coords, num)
-    coords.all? do |coord|
-      coord.scan(/\d+/).first == num
-    end
-  end
-
-  def consecutive?(coords)
-    no_row_gaps?(coords) || no_column_gaps?(coords)
-  end
-
-  def no_column_gaps?(coords)
-    columns = coords.map { |coord| coord[0] }
-
-    columns == (columns.first..columns.last).to_a
-  end
-
-  def no_row_gaps?(coords)
-    rows = coords.map { |coord| coord.scan(/\d+/).first }
-
-    rows == (rows.first..rows.last).to_a
-  end
-end
