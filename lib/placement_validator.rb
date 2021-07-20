4bfd035059773c8edfd39906ec375966b5c10921
Tanner Dale <tanner@tannerdale.com>
2021-07-20 16:02:49 -0500

    Add functionality to handle edge cases

--- lib/placement_validator.rb
+++ lib/placement_validator.rb
@@ -3,12 +3,23 @@ module PlacementValidator
 
   def valid?(coords)
     coords = coords.map { |coord| coord.bytes }
+
     coords[..-2].each_with_index.all? do |coord, i|
       next_coord = coords[i + 1]
 
-      result = [next_coord.first - coord.first, next_coord.last - coord.last]
+      result = get_result(coord, next_coord)
 
-      result == [1, 0] || result == [0, 1]
+      result.one? { |num| num == 1 }
+    end
+  end
+
+  def get_result(coord, next_coord)
+    coord.each_with_index.map do |coord2, i|
+      if coord2 == 57
+        1 if coord2 - next_coord[i] == 8
+      else
+        next_coord[i] - coord2
+      end
     end
   end
 end
